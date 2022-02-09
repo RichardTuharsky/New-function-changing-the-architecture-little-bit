41e912fdd4bc0c4774c942bb7f0e615ea521291d
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-02-09 13:35:14 +0100

    .

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cgitb import text
 from tkinter import *
+from tkinter import font
 import backend
 from tkinter.ttk import *
 
@@ -11,7 +12,7 @@ def add_command():
 
 master = Tk("Kniznica")
 master.wm_title("Kniznica")
-master.geometry("400x400")
+master.geometry("400x180")
 
 
 label = Label(master,text ="Dobry den, vytajte v nasej miestnej kniznici.")         
@@ -21,22 +22,52 @@ def openNewWindow():
 
 	newWindow = Toplevel(master)
 	newWindow.title("Knihy")
-	newWindow.geometry("400x400")
-	Label(newWindow,text ="Tu mozte najst informacie o knihach",width=33).pack() 
-	Button(newWindow,text ="Spusti kniznicu",width=20).pack()
-	Button(newWindow,text ="Vyhladaj novu knihu",width=20).pack()
-	Button(newWindow,text ="Vymaz knihu",width=20).pack()
-	Button(newWindow,text ="Zadaj novu knihu",width=20).pack()
-	Button(newWindow,text ="Vrat knihu do kniznice",width=20).pack()	
+	newWindow.geometry("500x400")
+	l1 = Label(newWindow, text = "Vymaz knihu")
+	l1.grid(row=0, column=3)
+	vymaz_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = vymaz_knihu)
+	ent1.grid(row = 0, column = 4)
+	vymaz_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = vymaz_knihu)
+	ent1.grid(row = 0, column = 10)
+	vymaz_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = vymaz_knihu)
+	ent1.grid(row = 0, column = 15)
+	l1 = Label(newWindow, text = "Zadaj novu knihu")
+	l1.grid(row=5, column=3)
+	zadaj_novu_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = zadaj_novu_knihu)
+	ent1.grid(row = 5, column = 4)
+	zadaj_novu_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = zadaj_novu_knihu)
+	ent1.grid(row = 5, column = 10)
+	zadaj_novu_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = zadaj_novu_knihu)
+	ent1.grid(row = 5, column = 15)
+	l1 = Label(newWindow, text = "Vyhladaj knihu")
+	l1.grid(row=10, column=3)
+	vyhladaj_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = vyhladaj_knihu)
+	ent1.grid(row = 10, column = 4)
+	vyhladaj_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = vyhladaj_knihu)
+	ent1.grid(row = 10, column = 10)
+	vyhladaj_knihu = StringVar()
+	ent1 = Entry(newWindow, textvariable = vyhladaj_knihu)
+	ent1.grid(row = 10, column = 15)
 	
+
+
 btn = Button(master,text ="Informacie o knihach",command = openNewWindow)  
 btn.pack(pady = 18)
+	
 
 
 def openNewWindow():
     
 	newWindow = Toplevel(master)
-	newWindow.title("Knihy")
+	newWindow.title("Uzivatel")
 	newWindow.geometry("400x400")
 	Label(newWindow,text ="Tu mozte najst informacie o uzivatelovi",width=35).pack()
 	Button(newWindow,text ="Vytvor noveho uzivatela",width=21).pack()
@@ -50,5 +81,6 @@ btn.pack(pady = 18)
 
 mainloop()
 
-window.mainloop()
 
+
+	
